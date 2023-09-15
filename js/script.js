af1d8ee664d8ace6b570830e38af4a559e78a98f
Eric Manley <75515024+ErkFX@users.noreply.github.com>
2023-09-15 14:06:30 -0500

    Add all main features with CSS and some JS
    Added HTML for the clicker area as well as the upgrade area.
    Added CSS to make the background darker and the buttons lighter.
    Added :hover to make buttons get lighter on hovering.
    Added :hover on tree to make it grow larger.
    Made the tree shrink when being clicked on.
    Logs increase by 1000 per click only for testing purposes.
    Lumberjacks increase logs per second by 1.
    Axes increase logs per second by 5.
    Chainsaws increase logs per second by 25.
    Added an interval function to save the game every 30 seconds.
    Added a loading function that will load where you last left off in
    the game when refreshing or loading the browser

---
+++ js/script.js
@@ -0,0 +1,119 @@
+let clickingPower = 1000;
+let logs = 0;
+let lumberjackCost = 15;
+let lumberjacks = 0;
+let axeCost = 100;
+let axes = 0;
+let chainsawCost = 1000;
+let chainsaws = 0;
+let logsPerSecond = 0;
+
+function addToLogs(amount) {
+    logs = logs + amount;
+    document.getElementById("logs").innerHTML = logs;
+}
+
+function updateLogsPerSecond() {
+    logsPerSecond = lumberjacks + (axes * 5) + (chainsaws * 25);
+    document.getElementById("logsPerSecond").innerHTML = logsPerSecond;
+}
+
+function buyLumberjack() {
+    if (logs >= lumberjackCost) {
+        logs -= lumberjackCost;
+        lumberjacks++;
+        lumberjackCost = Math.round(lumberjackCost * 1.15);
+
+        document.getElementById("logs").innerHTML = logs;
+        document.getElementById("lumberjackCost").innerHTML = lumberjackCost;
+        document.getElementById("lumberjacks").innerHTML = lumberjacks;
+        updateLogsPerSecond();
+    }
+}
+
+function buyAxe() {
+    if (logs >= axeCost) {
+        logs -= axeCost;
+        axes++;
+        axeCost = Math.round(axeCost * 1.15);
+
+        document.getElementById("logs").innerHTML = logs;
+        document.getElementById("axeCost").innerHTML = axeCost;
+        document.getElementById("axes").innerHTML = axes;
+        updateLogsPerSecond();
+    }
+}
+
+function buyChainsaw() {
+    if (logs >= chainsawCost) {
+        logs -= chainsawCost;
+        chainsaws++;
+        chainsawCost = Math.round(chainsawCost * 1.15);
+
+        document.getElementById("logs").innerHTML = logs;
+        document.getElementById("chainsawCost").innerHTML = chainsawCost;
+        document.getElementById("chainsaws").innerHTML = chainsaws;
+        updateLogsPerSecond();
+    }
+}
+
+// Because the saveGame is saved in a JSON file, it has been parsed. We now need to unparse the gameSave. You use getItem to get the item out that you set with .setItem in the save function.
+
+// all of the if statements are checking to see if the type of each savedGame files type is not undefined. If they are not undefined, the variables that run the game will be set to = the savedGame variables.
+
+// you will need to add an if statement for every variable that you have saved just like this.
+function loadGame() {
+    let savedGame = JSON.parse(localStorage.getItem("gameSave"));
+    if (typeof savedGame.logs !== "undefined") logs = savedGame.logs;
+    if (typeof savedGame.clickingPower !== "undefined") clickingPower = savedGame.clickingPower;
+    if (typeof savedGame.lumberjackCost !== "undefined") lumberjackCost = savedGame.lumberjackCost;
+    if (typeof savedGame.lumberjacks !== "undefined") lumberjacks = savedGame.lumberjacks;
+    if (typeof savedGame.axeCost !== "undefined") axeCost = savedGame.axeCost;
+    if (typeof savedGame.axes !== "undefined") axes = savedGame.axes;
+    if (typeof savedGame.chainsawCost !== "undefined") chainsawCost = savedGame.chainsawCost;
+    if (typeof savedGame.chainsaws !== "undefined") chainsaws = savedGame.chainsaws;
+}
+
+// The names on the left of the ":" are going to be the folder names. logs: is basically the score folder while clickingPower: is the clickingPower folder.
+function saveGame() {
+    let gameSave = {
+        logs: logs,
+        clickingPower: clickingPower,
+        lumberjackCost: lumberjackCost,
+        lumberjacks: lumberjacks,
+        axeCost: axeCost,
+        axes: axes,
+        chainsawCost: chainsawCost,
+        chainsaws: chainsaws
+    };
+    //localStorage = cookies in the browser. The rest of the code stores the gameSave in the cookies, JSON.stringify turns the gameSave into a string. JSON files are a file format that stores variables. Putting it in a JSON inputs a parse.
+    localStorage.setItem("gameSave", JSON.stringify(gameSave));
+}
+
+// This will load the game when you refresh or open the webpage. This will also update logs per second and all purchased upgrades.
+
+window.onload = function() {
+    loadGame();
+    updateLogsPerSecond();
+    document.getElementById("logs").innerHTML = logs;
+    document.getElementById("lumberjackCost").innerHTML = lumberjackCost;
+    document.getElementById("lumberjacks").innerHTML = lumberjacks;
+    document.getElementById("axeCost").innerHTML = axeCost;
+    document.getElementById("axes").innerHTML = axes;
+    document.getElementById("chainsawCost").innerHTML = chainsawCost;
+    document.getElementById("chainsaws").innerHTML = chainsaws;
+}
+
+setInterval(function() {
+    logs += lumberjacks;
+    logs += axes * 5;
+    logs += chainsaws * 25;
+    document.getElementById("logs").innerHTML = logs;
+    document.title = logs + " log(s) - Tree Clicker";
+}, 1000); //1000ms = 1 second
+
+//Every 30 seconds we want the game to save so we put the gameSave() function into a setInterval and set it to 30000ms
+
+setInterval (function() {
+    saveGame();
+}, 30000); //30000ms = 30 seconds
